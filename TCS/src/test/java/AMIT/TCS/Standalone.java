03da74a875df0ba5dadc58b8d848841cf7147c2f
AMIT <AMIT@DESKTOP-5K17L9B>
2025-07-02 23:42:54 -0700

    Second test

--- TCS/src/test/java/AMIT/TCS/Standalone.java
+++ TCS/src/test/java/AMIT/TCS/Standalone.java
@@ -69,8 +69,58 @@ public class Standalone extends BaseTest {
 	String mess= p1.Thankss();
 	//String Thanksmessage= driver.findElement(By.cssSelector(".hero-primary")).getText();
 	Assert.assertEquals(mess, "THANKYOU FOR THE ORDER.");
-	System.out.println(mess);
-	
-		
+	System.out.println(mess);		
 }
+		@Test
+		public void stand2() {
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			String prod1="ADIDAS ORIGINAL";
+		L.credentials("am@gmail.com", "Automation@9898");
+		CartPage c= new CartPage(driver);
+		 c.Allprod();
+		 c.productFind(prod1);
+		//driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("am@gmail.com");
+		//driver.findElement(By.xpath("//input[@id='userPassword']")).sendKeys("Automation@9898");
+		//driver.findElement(By.xpath("//input[@type='submit']")).click();
+		//WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".col-lg-4")));
+	//List<WebElement> Productss= driver.findElements(By.cssSelector(".col-lg-4"));
+		//WebElement prod= Productss.stream().filter(product->product.findElement(By.cssSelector("b")).getText().equals("ADIDAS ORIGINAL")).findFirst().orElse(null);
+		//prod.findElement(By.cssSelector(".card-body button:nth-of-type(2)")).click();
+		//(//div[@class='card-body'])[2]//button this will work
+		
+		
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#toast-container")));
+		//instead of invisibilityofelementlocated use this invisibilty of to overcome perfomance issue
+		//wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector(".ng-animating"))));
+		 c.productClick(prod1);
+		 CatalogPage C1= new CatalogPage(driver);
+		 boolean cartprod= C1.Afteradding(prod1);
+		 Assert.assertTrue(cartprod);
+		 C1.checkoutButton();
+		 //for git
+		 //hang///
+		//driver.findElement(By.xpath("//button[@routerlink='/dashboard/cart']")).click();
+		//List<WebElement> cartt=driver.findElements(By.xpath("(//div[@class='cart']//h3)[1]"));
+		 //boolean Cartprod= cartt.stream().anyMatch(cart->cart.getText().equalsIgnoreCase(prod1));
+		//Assert.assertTrue(Cartprod);
+		 //directly can use equalingorecase then question is in first stream ca we use it?
+		//driver.findElement(By.xpath("//div[@class='subtotal cf ng-star-inserted']//button")).click();
+		 Paymentpage p = new Paymentpage(driver);
+		 p.countryclick("India");
+		//driver.findElement(By.cssSelector(".form-group input")).sendKeys("India");
+		//Actions a = new Actions(driver);
+		
+		//WebElement dropdownIndia= driver.findElement(By.cssSelector(".ta-results button:nth-of-type(2)"));
+		
+		//a.moveToElement(dropdownIndia).click().build().perform();
+		//WebElement submit= driver.findElement(By.cssSelector(".btnn i"));
+		//a.moveToElement(submit).click().build().perform();
+		 PlacedPage p1= new PlacedPage(driver);
+		String mess= p1.Thankss();
+		//String Thanksmessage= driver.findElement(By.cssSelector(".hero-primary")).getText();
+		Assert.assertEquals(mess, "THANKYOU FOR THE ORDER. i");
+		System.out.println(mess);
+			
+		}
 }
